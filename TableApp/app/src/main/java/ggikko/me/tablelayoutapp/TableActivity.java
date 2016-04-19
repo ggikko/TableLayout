8b1cb89ab1e16bb83aa318e19785dd02521f4abe
jihongPark <ggikko@hotmail.com>
2016-04-19 23:05:02 +0900

    add ignore & init

---
+++ TableApp/app/src/main/java/ggikko/me/tablelayoutapp/TableActivity.java
@@ -0,0 +1,15 @@
+package ggikko.me.tablelayoutapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TableActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_table);
+
+        
+    }
+}
